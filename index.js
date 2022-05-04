8c3fb9ad48ccd76966a3eefe396429610f686749
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-04 07:52:45 +0600

    data loaded on the server

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// connect to database
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://user1:aGdheVxCvvWm8Q0i@cluster0.ungi5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// main run function
+async function run() {
+    try {
+      await client.connect();
+      const inventoryCollection = client.db("autohunt").collection("inventories");
+
+      app.get('/inventory', async (req, res)=>{
+          const query = {};
+          const cursor = inventoryCollection.find(query);
+          const result = await cursor.toArray();
+          res.send(result);
+      })
+    } finally {
+    //   await client.close();
+    }
+  }
+run().catch(console.dir);
 
 app.get('/', (req, res)=>{
     res.send('server is running');
