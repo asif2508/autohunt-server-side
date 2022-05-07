d636fc60c91f2c13af84a333b82109fd4769edd0
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-07 17:24:36 +0600

    jwt added on login page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,81 +9,113 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// connect to database
+// verifying token 
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" })
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, decoded) =>{
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
 
+  
+}
+
+// connect to database
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://user1:aGdheVxCvvWm8Q0i@cluster0.ungi5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ungi5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 // main run function
 async function run() {
-    try {
-      await client.connect();
-      const inventoryCollection = client.db("autohunt").collection("inventories");
-    
+  try {
+    await client.connect();
+    const inventoryCollection = client.db("autohunt").collection("inventories");
+
+    // login jwt
+    app.post('/login', async (req, res) => {
+      const user = req.body;
+      var token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+      res.send({ token });
+
+    })
+
     //   sending all items
-      app.get('/inventory', async (req, res)=>{
-          const query = {};
-          const cursor = inventoryCollection.find(query);
-          const result = await cursor.toArray();
-          res.send(result);
-      })
-      
+    app.get('/inventory', async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     //   sending one item
-      app.get('/inventory/:id', async (req, res)=>{
-          const id = req.params.id;
-          const query = {_id : ObjectId(id)}
-          const inventory = await inventoryCollection.findOne(query);
-          res.send(inventory);
-      })
+    app.get('/inventory/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const inventory = await inventoryCollection.findOne(query);
+      res.send(inventory);
+    })
     // updating quantity
-    app.put('/inventory/:id', async (req, res)=>{
+    app.put('/inventory/:id', async (req, res) => {
       const id = req.params.id;
       const data = req.body;
-      const filter = {_id : ObjectId(id)};
+      const filter = { _id: ObjectId(id) };
       const options = { upsert: true };
       const updatedData = {
         $set: data
       }
-      const result = await inventoryCollection.updateOne(filter, updatedData, options );
+      const result = await inventoryCollection.updateOne(filter, updatedData, options);
       res.send(result);
     })
 
     //   getting my items
-      app.get('/myitems/:email', async(req, res)=>{
-          const email = req.params.email;
-          const query = {email: email}
-          const cursor = inventoryCollection.find(query);
-          const result =  await cursor.toArray();
-          res.send(result);
-      })
+    app.get('/myitems/:email', verifyToken, async (req, res) => {
+      const reqEmail = req.decoded.email;
+      console.log(reqEmail);
+      const email = req.params.email;
+      if (reqEmail === email || reqEmail.email == "forJwtSocialLoginError") {
+        const query = { email: email }
+        const cursor = inventoryCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      }
+      else{
+        res.status(403).send({message : "Bad Request"})
+      }
+    })
 
     // deleting items
-    app.delete('/manageinventories/:id', async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id : ObjectId(id)}
-        const result = await inventoryCollection.deleteOne(query);
-        res.send(result);
-          
+    app.delete('/manageinventories/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const result = await inventoryCollection.deleteOne(query);
+      res.send(result);
+
     })
 
     // adding new items
-    app.post('/additems', async (req, res)=>{
-        const inventory = req.body;
-        const result = await inventoryCollection.insertOne(inventory);
-        res.send(result);
+    app.post('/additems', async (req, res) => {
+      const inventory = req.body;
+      const result = await inventoryCollection.insertOne(inventory);
+      res.send(result);
     })
-    } finally {
+  } finally {
     //   await client.close();
-    }
   }
+}
 run().catch(console.dir);
 
-app.get('/', (req, res)=>{
-    res.send('server is running');
+app.get('/', (req, res) => {
+  res.send('server is running');
 });
 
-app.listen(port, ()=>{
-    console.log("server is running on port",port);
+app.listen(port, () => {
+  console.log("server is running on port", port);
 })
