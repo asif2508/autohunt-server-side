6ce38e1a3ab439621a6a2e19e3b263f92a38afb7
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-08 17:52:19 +0600

    status code updated

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 // using middle wares
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 // verifying token 
